9dcf68009b04650493f1829711c8560f2a5cf29a
Arnau478 <arnauxabia@gmail.com>
2024-10-27 20:29:19 +0100

    Fix Node className trailing space

--- src/Graph/Node.tsx
+++ src/Graph/Node.tsx
@@ -1,7 +1,7 @@
 function Node({ selected, onClick, pos }: { selected: boolean, onClick: (e: React.MouseEvent) => void, pos: { x: number, y: number } }) {
   return (
     <circle
-      className={"node" + " " + (selected ? "selected" : "")}
+      className={["node", ...(selected ? ["selected"] : [])].join(' ')}
       cx={pos.x} cy={pos.y}
       r="10"
       onClick={onClick}
